c000280375212de840985e9007201080362af0ef
Mike iLL Kilmer <mike@mzoo.org>
2015-08-17 18:20:05 -0500

    Play around with adding a button.

--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,7 @@
 <header class="banner" role="banner">
+	<?php if(is_home() || is_front_page()) : ?>
+		<div class="greeting">
+	<?php endif ?>
   <div class="container">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
     <nav role="navigation">
@@ -8,5 +11,14 @@
       endif;
       ?>
     </nav>
+	<?php if(is_home() || is_front_page()) : ?>
+			<div class="free_consultation">
+				<h2>Our pleasure</h2>
+				<p>Is to offer you a 
+				<a class="btn btn-default" href="#">Free Consultation</a></p>
+			</div>
+		</div>
+	<?php endif ?>
   </div>
+
 </header>
