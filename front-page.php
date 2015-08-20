1cc454aa66eb6c18c02db1c9688fb0b813ae3129
Mike iLL Kilmer <mike@mzoo.org>
2015-08-19 19:58:23 -0500

    Responsive matching height fluid divs based on JS fiddle. Using Wells.

--- front-page.php
+++ front-page.php
@@ -8,43 +8,108 @@
   <?php //get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 
-  <div class="container-fluid container-eq-height">
-  	<div class="row-fluid row-eq-height">
-			<div class="col-xs-12 col-sm-6" style="background-color:yellow;"><?php the_field( 'intro' ) ?></div>
-			<div class="col-xs-12 col-sm-6" style="background-color:magenta"> <h2>FAQ</h2>
-			
-			<?php 
-				$posts = get_posts(array(
-					'posts_per_page'	=> -1,
-					'post_type'			=> 'faq'
-				));
 
-				if( $posts ): ?>
-	
-					<ul>
-		
-					<?php foreach( $posts as $post ): 
-		
-						setup_postdata( $post )
-		
-						?>
-						<li>
-							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-						</li>
-	
-					<?php endforeach; ?>
-	
-					</ul>
-	
-					<?php wp_reset_postdata(); ?>
+		<!-- top row of wells or rounded corner divs -->
 
-				<?php endif; ?>
+		<div class="row col-wrap">
+			<div class="col-sm-6 col">
+				<div class="well">
+					<?php the_field( 'intro' ) ?>
 				</div>
-		</div>
-	</div>
+			</div><!-- end span 6-->
 		
-		<div class="row-fluid row-eq-height">
-			<div class="col-sm-12" style="background-color:grey;"><?php the_field( 'intro' ) ?></div>
+			<div class="col-sm-6 col">
+				<div class="well">
+				<h2>FAQ</h2>
 			
-		</div>
+					<?php 
+						$posts = get_posts(array(
+							'posts_per_page'	=> -1,
+							'post_type'			=> 'faq'
+						));
+
+						if( $posts ): ?>
+	
+							<ul>
+		
+							<?php foreach( $posts as $post ): 
+		
+								setup_postdata( $post )
+		
+								?>
+								<li>
+									<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+								</li>
+	
+							<?php endforeach; ?>
+	
+							</ul>
+	
+							<?php wp_reset_postdata(); ?>
+
+						<?php endif; ?>
+				</div>
+			</div><!-- end span 6-->
+			 
+		</div><!-- end row -->
+		
+
+		<div class="row base col-wrap">
+		<!-- add a second row of wells or rounded corner divs immeadiately underneath-->
+		<div class="col-sm-6 col-base"><div class="well"></div></div>
+		<div class="col-sm-6 col-base"><div class="well"></div></div>
+		</div><!-- close row -->
+		
+
+		<!-- top row of wells or rounded corner divs -->
+
+		<div class="row col-wrap">
+			<div class="col-sm-6 col">
+				<div class="well">
+					<?php the_field( 'intro' ) ?>
+				</div>
+			</div><!-- end span 6-->
+		
+			<div class="col-sm-6 col">
+				<div class="well">
+				<h2>FAQ</h2>
+			
+					<?php 
+						$posts = get_posts(array(
+							'posts_per_page'	=> -1,
+							'post_type'			=> 'faq'
+						));
+
+						if( $posts ): ?>
+	
+							<ul>
+		
+							<?php foreach( $posts as $post ): 
+		
+								setup_postdata( $post )
+		
+								?>
+								<li>
+									<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+								</li>
+	
+							<?php endforeach; ?>
+	
+							</ul>
+	
+							<?php wp_reset_postdata(); ?>
+
+						<?php endif; ?>
+				</div>
+			</div><!-- end span 6-->
+			 
+		</div><!-- end row -->
+		
+
+		<div class="row base col-wrap">
+		<!-- add a second row of wells or rounded corner divs immeadiately underneath-->
+		<div class="col-sm-6 col-base"><div class="well"></div></div>
+		<div class="col-sm-6 col-base"><div class="well"></div></div>
+		</div><!-- close row -->
+  	
 <?php endwhile; ?>
