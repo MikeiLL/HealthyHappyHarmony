c943d1116b7db7e5c26126c730c8f51887c0db44
Mike iLL Kilmer <mike@mzoo.org>
2015-09-14 18:23:52 -0500

    Display articles in columns.

--- front-page.php
+++ front-page.php
@@ -21,20 +21,7 @@
 		<!-- top row of wells or rounded corner divs -->
 
 		<div class="row col-wrap">
-				<div class="well full-width-well-dark">
-					<?php get_template_part('templates/front-page/articles'); ?>
-				</div>
-		
-			 
+				<?php get_template_part('templates/front-page/articles'); ?>		
 		</div><!-- end row -->
-		
-
-
-  	
-  	<div class="border-box">
-  
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lacinia, lacus nec laoreet interdum, dui sem aliquet leo, a egestas ipsum massa vitae augue. Vestibulum et interdum augue. Phasellus et nisi metus. Donec accumsan quis velit at varius. Suspendisse tincidunt eu orci eget tincidunt. In consectetur tincidunt lectus. Vestibulum mollis, dui a congue pharetra, ligula mauris venenatis nibh, non tincidunt turpis nisl in mi. Nam placerat erat non justo viverra dictum. Donec arcu arcu, interdum et dictum ac, egestas et nibh. Cras tempor, quam sit amet euismod aliquam, erat ex pretium erat, pellentesque dapibus arcu nulla eu tellus. Sed non sapien ultricies, bibendum ipsum vitae, aliquam metus. Morbi interdum ullamcorper turpis at volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse tincidunt dictum nisl ut vehicula.</p>
-  
-		</div>
 
 <?php endwhile; ?>
--- templates/front-page/articles.php
+++ templates/front-page/articles.php
@@ -1,27 +1,26 @@
-<div class="col">
+			<h2>Articles</h2>
+		
+				<?php 
+				
+						$args = array(
+			'posts_per_page' => 3,
+			'meta_key' => '_featured-post',
+			'meta_value' => 1
+			);
+			$featured = new WP_Query($args);
 
-				<h2>Resources</h2>
-			
-					<?php 
-					
-						  $args = array(
-        'posts_per_page' => 5,
-        'meta_key' => '_featured-post',
-        'meta_value' => 1
-				);
-				$featured = new WP_Query($args);
- 
-				if ($featured->have_posts()): while($featured->have_posts()): $featured->the_post(); ?>
+			if ($featured->have_posts()): while($featured->have_posts()): $featured->the_post(); ?>
+				<div class="col-sm-4 col">
+					<div class="well">
 				<h3><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h3>
-				<p class="details">By <a href="<?php the_author_posts() ?>"><?php the_author(); ?> </a> / On <?php echo get_the_date('F j, Y'); ?> / In <?php the_category(', '); ?></p>
 				<?php if (has_post_thumbnail()) : ?>
- 
-				<figure> <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('', array('class' => 'box')); ?></a> </figure>
-				<p ><?php the_excerpt();?></p>
-				<?php
-				endif;
-				endwhile; else:
-				endif;
-				?>
+					<figure> <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumb', array( 'class'	=> "img-responsive img-center")); ?></a> </figure>
+				<?php endif; ?>
+				<p ><?php echo the_excerpt();?></p>
+				</div>
+			</div>
+			<?php endwhile; else:
+			endif;
+			?>
 
-			</div><!-- end span 6-->
+		</div><!-- end span 6-->
