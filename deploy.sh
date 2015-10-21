676197ab98e78f3716a28d3775092d24d7ca4421
Mike iLL Kilmer <mike@mzoo.org>
2015-10-21 17:00:36 -0500

    Correct theme directory.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 gulp --production &&
-rsync -avP dist lib screenshot.png templates *.php *.css hamiltonwellness:public_html/wp/wp-content/themes/poman
+rsync -avP dist lib screenshot.png templates *.php *.css hamiltonwellness:public_html/wp/wp-content/themes/hhharmony
