7af58eeff548fb729aecc040a0ef9f55240063a2
Mike iLL Kilmer <mike@mzoo.org>
2015-10-22 13:04:05 -0500

    Make title in archive page link to article.

--- templates/content.php
+++ templates/content.php
@@ -4,12 +4,12 @@
    	 <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
    	<?php else: ?>
    	<?php $content_class = "large_text_display"; ?>
-   	 <h2 class="entry-title"><?php the_title(); ?></h2>
+   	 <a class="archive_title" href="<?=the_permalink()?>"><h2 class="entry-title"><?php the_title(); ?></h2></a>
    	<?php endif ?>
     <?php if ($posts[0]->post_type == 'post')
       		get_template_part('templates/entry-meta'); ?>
   </header>
   <div class="entry-summary <?php echo $content_class ? $content_class : '' ?> ">
-    	<?php the_content(); ?>
+    <?php the_content(); ?>
   </div>
 </article>
