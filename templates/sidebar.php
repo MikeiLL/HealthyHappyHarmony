4d81aa481b50dfd34af072c7b7653d018d5f3c2a
Mike iLL Kilmer <mike@mzoo.org>
2015-11-03 19:02:04 -0600

    Add the correct linked in link and style it.

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,1 +1,4 @@
 <?php dynamic_sidebar('sidebar-primary'); ?>
+	<div class="linkedin_sidebar">
+		<a href="https://www.linkedin.com/company/hamilton-resources" target="_blank">Meet me on Linked <i class="fa fa-linkedin fa-2x"></i></a>
+	</div>
