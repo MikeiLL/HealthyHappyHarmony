8852c4e2fe7b68c48525451dec069c1f1cb1b4e9
Mike iLL Kilmer <mike@mzoo.org>
2015-12-02 16:09:22 -0600

    Insert Sheri pic instead of clip art.

--- templates/front-page/contact.php
+++ templates/front-page/contact.php
@@ -1,20 +1,23 @@
 		<div class="col-sm-4 col">
 				<div class="well">
-				<a href="contact/"><span class="featured_items glyphicon glyphicon-record featured_item_icons text-center"></span></a>
-				<div class="no_color">
-					<a href="contact/"><h2 class="front-page-heading text-center">Contact Us</h2></a>
-				</div>
-				
-							<a href="contact"><img alt="contact us figure" class="img-responsive img-center" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icons/Contact.png"/></a>
+          <a href="contact/"><span class="featured_items glyphicon glyphicon-record featured_item_icons text-center"></span></a>
 
-							<h4 style="text-align:center"><a href="contact">Tell Us About Your Work</a></h4>
-							<p>&nbsp;</p>
+          <div class="no_color">
+            <a href="contact/"><h2 class="front-page-heading text-center">Contact Us</h2></a>
+          </div>
 
-							<h4 style="text-align: center;">850 232-5175</h4>&nbsp;
+          <a href="contact"><img alt="contact us figure" class="img-responsive img-center front-contact-us" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/Sheri.png"/>
+          </a>
 
-							<p style="text-align: center;"><a href="mailto:sheri@hamiltonwellnessresources.com">sheri @ 
-							
-							<br/>hamiltonwellnessresources.com</a></p>
-																
+
+          <h4 class="front-contact-us-header" style="text-align:center"><a href="contact">Click here to contact us</a></h4>
+          <p>&nbsp;</p>
+
+          <h4 style="text-align: center;">850 232-5175</h4>&nbsp;
+
+          <p style="text-align: center;"><a href="mailto:sheri@hamiltonwellnessresources.com">sheri @
+
+          <br/>hamiltonwellnessresources.com</a></p>
+
 					</div> <!-- end well -->
 			</div><!-- end span 4-->
