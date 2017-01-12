c009562a3d221de7967a0e501afdef1b4d83fa3e
Mike iLL Kilmer <mike@mzoo.org>
2017-01-12 10:03:03 -0600

    Remove container class from footer div.

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <?php use Roots\Sage\Extras; ?>
 
 <footer class="content-info" role="contentinfo">
-  <div class="container">
+  <div>
   	<h3 class="secondary-tagline">Healthy &bull; Happy &bull; Harmony</h3>
   	<div id="share"></div>
     <?php dynamic_sidebar('sidebar-footer'); ?>
